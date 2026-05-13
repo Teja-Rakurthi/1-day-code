1d0a6c9a956345f836af4e8eb73f9e2639c7c306
Teja-Rakurthi <tejarakurthi@gmail.com>
2026-05-14 00:48:34 +0530

    Added day1 Java files

---
+++ 1stdayjava/main.java/main.java
@@ -0,0 +1,12 @@
+class person{
+    String name;
+    int age;
+}
+public class main {
+    public static void main(String [] args){
+        person p1= new person();
+        p1.name="teja";
+        p1.age=23;
+        System.out.println(p1.name);
+    }
+}
